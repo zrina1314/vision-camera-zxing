49d97e72bb3019e6ca72bf28e4bf4c5838339c0d
xulihang <xulihanghai@163.com>
2024-09-14 15:15:03 +0800

    fix: add the missing decodeBase64 in the .m file

--- ios/VisionCameraZXing.m
+++ ios/VisionCameraZXing.m
@@ -2,7 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(VisionCameraZXing, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+RCT_EXTERN_METHOD(decodeBase64:(NSString *)base64
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
